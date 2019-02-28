3b7dc84cc0e2dbaa8d8d968a314e80dd98ee9534
Josh Ryal <S525501@mail.nwmissouri.edu>
2019-02-28 09:10:55 -0600

    Added base python files

---
+++ degrees_modules.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 28 09:10:09 2019
+
+@author: s525501
+"""
+
---
+++ github_degrees.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 28 09:08:05 2019
+
+@author: s525501
+"""
+
+from github import Github
+import networkx as nx
+import matpl0tlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
